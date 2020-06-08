68a62b926305565e56564e462c2eafc25e9d097b
sandhyacp <sandhya.channapura@gmail.com>
2020-06-08 16:17:06 -0500

    Remove unnecessary Console.Log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log("Client side JS file loaded!")
-
-
-
 const weatherForm = document.querySelector('form')
 const searchLocation = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
